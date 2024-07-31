c2194a91ca7a1a474d29d31a5da2873c468ded1f
ChristopherAnaya <chrisanaya32109@gmail.com>
2024-07-31 08:33:45 -0700

    Update Simple Calculator.py
    Added Addition

---
+++ Simple Calculator.py
@@ -0,0 +1,5 @@
+num1 = float(input("What is your first number?"))
+num2 = float(input("What is your second number?"))
+opp = input("Which of the following 4 signs would u like to use? +-*/ ")
+if opp == "+":
+    print(num1 + num2)
