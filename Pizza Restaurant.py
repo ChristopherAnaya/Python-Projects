0c6bceccee77f664c20f6a92ab6d8d8c13a0dcca
ChristopherAnaya <chrisanaya32109@gmail.com>
2024-07-31 10:19:44 -0700

    Finished Pizza Restaurant
    added plural and non plural forms

--- Pizza Restaurant.py
+++ Pizza Restaurant.py
@@ -1,14 +1,28 @@
 people = int(input("How many people will be in your party"))
+print("You will need...")
 
 large = int(people / 7)
 large_ex = int(people) % 7
 medium = int(large_ex / 3)
-medium_ex = int(medium) % 3
+medium_ex = int(large_ex) % 3
 
-print("You will need...")
 string1 = ("{} large pizzas")
+alt_string1 = ("1 large pizza")
 string2 = ("{} medium pizzas")
-string3 = ("& {} small pizzas")
-print(string1.format(large))
-print(string2.format(medium))
-print(string3.format(medium_ex))
+alt_string2 = ("1 medium pizza")
+string3 = ("{} small pizzas")
+alt_string3 = ("1 small pizza")
+
+if large == 1:
+    print(alt_string1)
+else:
+    print(string1.format(large))
+if medium == 1:
+    print(alt_string2)
+else:
+    print(string2.format(medium))
+if medium_ex == 1:
+    print(alt_string3)
+else:
+    print(string3.format(medium_ex))
+
