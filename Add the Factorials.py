0218c3be03967afaa9b1b8520caefd7862fe187d
ChristopherAnaya <chrisanaya32109@gmail.com>
2024-08-06 09:56:47 -0700

    Update Add the Factorials.py
    Adds the factorials of the range of 1 to a chosen number

--- Add the Factorials.py
+++ Add the Factorials.py
@@ -1,6 +1,11 @@
-number = 4
-total = 1
-while number >0:
-    total *= number
-    number -=1
-print(total)
+def fact(n):
+    total = 1
+    while n >0:
+        total *= n
+        n -=1
+    return total
+number = int(input("Choose a number."))
+new = 0
+for i in range(1,number + 1):
+    new += fact(i)
+print(new)
