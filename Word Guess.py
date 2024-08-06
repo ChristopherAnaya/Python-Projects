5eb060699948272f5cf3eecc2c79237859b38448
ChristopherAnaya <chrisanaya32109@gmail.com>
2024-08-06 10:33:32 -0700

    Update Word Guess.py

---
+++ Word Guess.py
@@ -0,0 +1,7 @@
+import random
+bank = ["disco", "requirement", "freshman", "courtesy", "vertical", "contact", "important", "discover", "influence", "absent", "joystick", "wilderness"]
+word = random.choice(bank)
+print(word)
+chances = 5
+for len in "hello":
+    print(2)
