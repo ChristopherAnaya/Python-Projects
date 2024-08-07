cad08b6833a7962a902813fba27f4027f29d9c8a
ChristopherAnaya <chrisanaya32109@gmail.com>
2024-08-07 09:10:45 -0700

    Update Word Guess.py
    This is a holing place while i work on the while loop

--- Word Guess.py
+++ Word Guess.py
@@ -1,7 +1,20 @@
 import random
 bank = ["disco", "requirement", "freshman", "courtesy", "vertical", "contact", "important", "discover", "influence", "absent", "joystick", "wilderness"]
 word = random.choice(bank)
-print(word)
+wordlisted = list(word)
+empty = ""
+for x in wordlisted:
+    empty += "_"
 chances = 5
-for len in "hello":
-    print(2)
+wrong = ""
+solved = len(word)
+stop = 1
+print("The word is " + str(len(word)) + " letters long.")
+print(word)
+
+
+
+if solved == 0:
+    print("You win")
+if chances == 0:
+    print("You lose")
