690427abdbc17a8485b14d917c42df692df9ce4d
ChristopherAnaya <chrisanaya32109@gmail.com>
2024-08-14 07:38:10 -0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,29 +1,32 @@
 def find_glasses(lst):
     lst = [x.lower() for x in lst]
     empty = []
-    stop = 0
-    lose = False
+    stop = 1
+    lose = 1
     for x in lst:
         hold = x
-        if "o" in x:
-            position = x.find("o")
-            x = list(x)
-            useless = x.pop(position)
-            x = "".join(x)
-            position_2 = x.find("o") + 1
-            for x in range(position + 1, position_2):
-                stop += 1
-                print(stop)
-            for y in range(position + 1, position_2):
-                while stop > 0:
-                    if hold[y] == "-":
-                        stop -= 1
+        while stop > 0:
+            if "o" in x:
+                position = x.find("o")
+                x = list(x)
+                useless = x.pop(position)
+                x = "".join(x)
+                if "o" in x:
+                    position_2 = x.find("o") + 1
+                    for z in range(position + 1, position_2):
+                        stop += 1
+                    while stop > 0:
+                        for y in range(position + 1, position_2):
+                            if hold[y] != "-":
+                                lose = 0
+                                break
+                        break
+                    if lose == 0:
+                        lose = 1
                     else:
-                        stop = 0
-                        lose = True
-            if lose == True:
-                lose = False
-            else:
-                empty.append(hold)
+                        empty.append(hold)
+                hold = x
+            break
     return empty
-print(find_glasses(["O--O", "O----d-O"]))
+print(find_glasses(['phone', 'O-o', 'coins', 'keys']))
+
