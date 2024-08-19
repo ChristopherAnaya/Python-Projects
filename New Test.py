4167affb327000d0b436cd5c3cb7ab579f26a3a6
ChristopherAnaya <chrisanaya32109@gmail.com>
2024-08-19 10:33:43 -0700

    Update New Test.py

--- New Test.py
+++ New Test.py
@@ -1,16 +1,7 @@
-def queue_time(c):
-    stop = 0
-    time = 0
-    def till(z):
-        if z == 0:
-            del c[0]
-        else:
-            c[0] -= 1
-    while stop == 0:
-        if c[0] != 0:
-            time += 1
-        till(c[0])
-        if c == []:
-            stop = 1
-    return(time)
-print(queue_time([3,2]))
+def what_century(year):
+    return int(year[0:2])+ 1
+
+
+
+
+print(what_century("2000"))
