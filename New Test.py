b11d14585cebbaccf3e5d6ea2a7396617761647a
ChristopherAnaya <chrisanaya32109@gmail.com>
2024-08-22 07:55:48 -0700

    Update New Test.py

--- New Test.py
+++ New Test.py
@@ -1,24 +1,10 @@
-def to_weird_case(w):
-    wl = list(w.lower())
-    final = []
-    space_test = 0
-    for x in wl:
-        z = wl.index(x)
-        wl[z] = ""
-        z -= space_test
-        if x == " ":
-            final.append(x)
-            space_test += 1
-        elif z == 0:
-            final.append(x.upper())
-        elif z % 2 == 0:
-            final.append(x.upper())
-        else:
-            final.append(x)
-    finals = "".join(final)
-    return finals
-    
-
-
-
-print(to_weird_case('THIs iS a TEST'))
+def expanded_form(n):
+    n = list(str(n))
+    final = ""
+    for x in list(n):
+        if x != "0":
+            final += (x + "0" * (len(n) - n.index(x) - 1)) + " + "
+        n[n.index(x)] = " "
+        print(n)
+    return final[0:-3]
+print(expanded_form(4982342))
