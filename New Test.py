800aa2f4c15c6807df92a8f62c86f86970256eee
ChristopherAnaya <chrisanaya32109@gmail.com>
2024-09-03 10:33:58 -0700

    Update New Test.py

--- New Test.py
+++ New Test.py
@@ -1,83 +1,32 @@
-def bomb_has_been_planted(m, time):
-    kloc = []
-    the_return = True
-    for x in range(len(m)):
-        for y in range(len(m[0])):
-            if m[x][y] == "CT":
-                ctloc = [x,y]
-            if m[x][y] == "B":
-                bloc = [x,y]
-            if m[x][y] == "K":
-                kloc = [x,y]
-    dist = 0
-    while ctloc[0] != bloc[0] and ctloc[1] != bloc[1]:
-        if ctloc[0] < bloc[0] and ctloc[1] < bloc[1]:
-            ctloc[0] += 1
-            ctloc[1] += 1
-            dist += 1
-        if ctloc[0] < bloc[0] and ctloc[1] > bloc[1]:
-            ctloc[0] += 1
-            ctloc[1] -= 1
-            dist += 1
-        if ctloc[0] > bloc[0] and ctloc[1] < bloc[1]:
-            ctloc[0] -= 1
-            ctloc[1] += 1
-            dist += 1
-        if ctloc[0] > bloc[0] and ctloc[1] > bloc[1]:
-            ctloc[0] -= 1
-            ctloc[1] -= 1
-            dist += 1
-    dist += abs(ctloc[0] - bloc[0]) + abs(ctloc[1] - bloc[1])
-    if dist + 10 > time:
-        the_return = False
-        print("fail")
-    else:
-        return True
-    if the_return == False:
-        if kloc != []:
-            dist = 0
-            while ctloc[0] != kloc[0] and ctloc[1] != kloc[1]:
-                if ctloc[0] < kloc[0] and ctloc[1] < kloc[1]:
-                    ctloc[0] += 1
-                    ctloc[1] += 1
-                    dist += 1
-                if ctloc[0] < kloc[0] and ctloc[1] > kloc[1]:
-                    ctloc[0] += 1
-                    ctloc[1] -= 1
-                    dist += 1
-                if ctloc[0] > kloc[0] and ctloc[1] < kloc[1]:
-                    ctloc[0] -= 1
-                    ctloc[1] += 1
-                    dist += 1
-                if ctloc[0] > kloc[0] and ctloc[1] > kloc[1]:
-                    ctloc[0] -= 1
-                    ctloc[1] -= 1
-                    dist += 1
-            print(dist)
-            dist += abs(ctloc[0] - kloc[0]) + abs(ctloc[1] - kloc[1])
-            print(dist)
-
-
-        else:
-            return False
-
-
-
-
-
-
-
-
-
-
-print(bomb_has_been_planted([
-            ["0", "0", "0", "0", "0", "0"],
-            ["CT", "0", "0", "0", "0", "0"],
-            ["0", "0", "0", "0", "0", "B"],
-            ["0", "0", "0", "0", "0", "0"],
-            ["0", "0", "K", "0", "0", "0"],
-            ["0", "0", "0", "0", "0", "0"],
-            ["0", "0", "0", "0", "0", "0"],
-            ["0", "0", "0", "0", "0", "0"],
-            ["0", "0", "0", "0", "0", "0"]
-        ], 13))
+s = list("`~!@#$%?^&*()-_=+[]{}\\|;:\",./<>")
+l = list("qwertyuiopasdfghjklzxcvbnm")
+def top_3_words(t):
+    c = list(t)
+    t = []
+    for x in c:
+        x = x.lower()
+        if x not in l:
+            x = " "
+        t.append(x)
+    t = "".join(t)
+    return t
+    '''t = t.split()
+    z = []
+    for x in t:
+        x = list(x)
+        g = x.copy()
+        test = True
+        if g != []:
+            for r in list(g):
+                if r in l:
+                    test = False
+            if test == False:
+                g = "".join(g)
+                z.append(g)
+    b = dict.fromkeys(z, 0)
+    for x in z:
+        b[x] += 1
+    q = dict(sorted(b.items(), key=lambda item: item[1], reverse = True))
+    n = [x for x in q.keys()]
+    return n[0:3] if len(n) > 2 else n'''
+print(top_3_words("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"))
