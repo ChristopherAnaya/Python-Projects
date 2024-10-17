636115fc90065c5e525d2278b9de01a9a47dd16e
ChristopherAnaya <chrisanaya32109@gmail.com>
2024-10-17 10:34:16 -0700

    fg

--- Add the Factorials.py
+++ Add the Factorials.py
@@ -1,11 +1,1 @@
-def fact(n):
-    total = 1
-    while n >0:
-        total *= n
-        n -=1
-    return total
-number = int(input("Choose a number."))
-new = 0
-for i in range(1,number + 1):
-    new += fact(i)
-print(new)
+print((29)%35)
