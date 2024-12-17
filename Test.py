f27d805b64e3c887814ce4f7115c420d3f2498be
Christopher Anaya <chrisanaya32109@gmail.com>
2024-12-17 10:08:13 -0700

    testing

--- Test.py
+++ Test.py
@@ -1,1 +1,14 @@
-print(chr(84    ))
+letters = list("ABCDEFGHIJKLMNOPQRSTUVWQYZ")
+def sort_by_exclusion(words):
+    print(words)
+    first = sorted(words)[0][0]
+    del words[words.index(sorted(words)[0])]
+    print(letters.index(first))
+    print(first)
+    del letters[:letters.index(first)]
+    del letters[letters.index(first)]
+    print(letters)
+    print(words)
+
+
+print(sort_by_exclusion(["THE","QUICK","BROWN","FOX","JUMPS","OVER","THE","LAZY","DOG"]))
