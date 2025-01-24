58816c1f00787cc5d600ebd0c03e986445eadaff
Christopher Anaya <chrisanaya32109@gmail.com>
2025-01-24 08:09:05 -0700

    testing

--- Test.py
+++ Test.py
@@ -1,14 +1,3 @@
-letters = list("ABCDEFGHIJKLMNOPQRSTUVWQYZ")
-def sort_by_exclusion(words):
-    print(words)
-    first = sorted(words)[0][0]
-    del words[words.index(sorted(words)[0])]
-    print(letters.index(first))
-    print(first)
-    del letters[:letters.index(first)]
-    del letters[letters.index(first)]
-    print(letters)
-    print(words)
-
-
-print(sort_by_exclusion(["THE","QUICK","BROWN","FOX","JUMPS","OVER","THE","LAZY","DOG"]))
+def name(x):
+    return "".join([(f"{x}{str(y)} + ") for y in range(1, 6)])
+print(name(input("name")))
