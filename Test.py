1a583337a7a2a7acfb9a861be28f6686823d63ae
Christopher Anaya <chrisanaya32109@gmail.com>
2025-01-27 09:46:51 -0700

    yuh

--- Test.py
+++ Test.py
@@ -1,3 +1,17 @@
-def name(x):
-    return "".join([(f"{x}{str(y)} + ") for y in range(1, 6)])
-print(name(input("name")))
+def prime_factors(base):
+    factors = {}
+    d = 2
+    while d * d <= base:
+        print(d)
+        while (base % d) == 0:
+            if d not in factors:
+                factors[d] = 0
+            factors[d] += 1
+            base //= d
+        d += 1
+    if base > 1:
+        factors[base] = 1
+    return factors
+
+
+print(prime_factors(8))
